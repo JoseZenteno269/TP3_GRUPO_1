41155a9e4914fda5993049370ee240b740c7b600
Fvercellone <th3r34p3r719@gmail.com>
2026-09-17 20:12:53 -0300

    Modificar y listar

--- src/Dao/DaoCategorias.java
+++ src/Dao/DaoCategorias.java
@@ -3,7 +3,9 @@ package Dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import Entidades.Categorias;
 
@@ -63,7 +65,78 @@ public class DaoCategorias {
                 e.printStackTrace();
             }
         }
-
         return filas;
     }
+    
+    public int modificarCategoria(Categorias categoria) {
+
+        Connection cn = null;
+
+        int filas = 0;
+
+        try {
+
+            cn = DriverManager.getConnection(host + dbName, user, pass);
+
+            String query = "UPDATE Categorias SET Nombre = ? WHERE IdCategoria = ?";
+
+            PreparedStatement pst = cn.prepareStatement(query);
+
+            pst.setString(1, categoria.getNombre());
+            pst.setInt(2, categoria.getIdCategoria());
+
+            filas = pst.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (cn != null) {
+                    cn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return filas;
+    }
+    
+    public ArrayList<Categorias> listarCategorias() {
+
+        Connection cn = null;
+
+        ArrayList<Categorias> lista = new ArrayList<Categorias>();
+
+        try {
+
+            cn = DriverManager.getConnection(host + dbName, user, pass);
+
+            String query = "SELECT IdCategoria, Nombre FROM Categorias";
+
+            PreparedStatement pst = cn.prepareStatement(query);
+
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+
+                Categorias categoria = new Categorias();
+
+                categoria.setIdCategoria(rs.getInt("IdCategoria"));
+                categoria.setNombre(rs.getString("Nombre"));
+                lista.add(categoria);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (cn != null) {
+                    cn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return lista;
+    }
+    
+    
 }
