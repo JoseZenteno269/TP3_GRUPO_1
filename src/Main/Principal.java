dfd93f3856790db6327f2c6de42a23b5a8b41618
jere2003-lab <gabrieltortora450@gmail.com>
2026-09-19 11:29:06 -0300

    Hago el packeage de Main, la clase principal y el metodo de alta de categorias.

---
+++ src/Main/Principal.java
@@ -0,0 +1,38 @@
+package Main;
+
+import Dao.DaoCategorias;
+import Entidades.Categorias;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		DaoCategorias daoCat = new DaoCategorias();		
+		
+		Categorias[] categorias = 
+
+			{
+					new Categorias(1,"Lacteos"),
+					new Categorias(2,"Limpieza"),
+					new Categorias(3, "Electrodomesticos"),
+					new Categorias(4, "Indumentaria"),
+					new Categorias(5, "Bebidas")
+					
+			};
+		
+		for(Categorias cate : categorias)
+		{
+			int filas = daoCat.agregarCategoria(cate);
+			
+			if(filas == 1)
+			{
+				System.out.println("Categoria agregada con exito");
+			}
+			else 
+			{
+				System.out.println("Error al agregar categoria");
+			}
+		}
+		
+	}
+
+}
